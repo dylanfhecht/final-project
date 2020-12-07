5c11838b0ec918ebba7a96985a102b4356ad084f
dylanfhecht <dhecht@tulane.edu>
2020-12-07 00:10:41 -0800

    changed source

--- toon/source/common/SourcePath.cpp
+++ toon/source/common/SourcePath.cpp
@@ -6,7 +6,7 @@
 
 #include "SourcePath.h"
 
-std::string source_path = "/Users/dylanhecht/Documents/GitHub/module-shading-team-c/toon";
+std::string source_path = "/Users/dylanhecht/Documents/GitHub/final-project/toon";
 
 
 // "/Users/dylanhecht/Documents/GitHub/module-shading-team-c/toon";
